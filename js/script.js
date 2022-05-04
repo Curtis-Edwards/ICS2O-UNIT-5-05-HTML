48dc62639fbed47c003d7328cdce81f304f51391
Curtis-Edwards <99354904+Curtis-Edwards@users.noreply.github.com>
2022-05-04 13:28:42 -0400

    added javascrip

--- js/script.js
+++ js/script.js
@@ -8,8 +8,8 @@
  * Check service worker.
  */
  if (navigator.serviceWorker) {
-  navigator.serviceWorker.register("/ICS20-PWA-Template/sw.js", {
-    scope: "/ICS20-PWA-Template/",
+  navigator.serviceWorker.register("/ICS2O-UNIT-5-05-HTML/sw.js", {
+    scope: "/ICS2O-UNIT-5-05-HTML/",
   })
 }
 
@@ -17,3 +17,26 @@
  * Input.
  */
 
+function buttonClicked() {
+  
+  const sidea = parseInt(document.getElementById('side-a').value)
+  const sideb = parseInt(document.getElementById('side-b').value)
+  const sidec = parseInt(document.getElementById('side-c').value)
+
+  if (sidea == sideb || sideb == sidec || sidec == sidea) {
+    document.getElementById("output").innerHTML =
+  "Your triangle is an isosceles"
+  console.log("isosceles")
+  } 
+  if (sidea != sideb && sideb != sidec && sidec != sidea) {
+    document.getElementById("output").innerHTML =
+  "Your triangle is an scalene"
+  console.log("scalene")
+  }
+  if (sidea == sideb && sideb == sidec && sidec == sidea) {
+    document.getElementById("output").innerHTML =
+  "Your triangle is an equilateral"
+  console.log("equilateral")
+  }
+
+}
